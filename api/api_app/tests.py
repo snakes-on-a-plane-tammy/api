5a5e7dd36e0b0f96292ca571de5ec22090b481eb
tammytdo11 <tammytdo11@gmail.com>
2019-09-04 20:34:44 -0700

    trigger circle ci failing test

--- api/api_app/tests.py
+++ api/api_app/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ApiTests(TestCase):
+    def test_(self):
+        self.assertEqual(True, False)
+        # self.assertEqual(True, True)
